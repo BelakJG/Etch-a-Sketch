6ef1e0ca106cfaf51df99435bfd4c14ee2c02a39
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-17 12:00:35 -0500

    Made a grid of squares Need to make them dynamically size still

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Hello, World!");
+
+let numTiles = 16;
+const board = document.querySelector(".etchContainer");
+for (let i = 0; i < numTiles; i++) {
+    const tile = document.createElement("div");
+    tile.classList.add("etchTile");
+    board.append(tile);
+}
