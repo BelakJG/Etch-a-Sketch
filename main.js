67dc380ce613b13a66a01268a095531c2a8f95de
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-17 14:38:55 -0500

    Tiles now darken Tiles darken by 20% on mouseover

--- main.js
+++ main.js
@@ -5,5 +5,22 @@ const board = document.querySelector(".etchContainer");
 for (let i = 0; i < numTiles; i++) {
     const tile = document.createElement("div");
     tile.classList.add("etchTile");
+    tile.style.backgroundColor = "White"
+    tile.style.filter = "brightness(1)";
+
+    tile.addEventListener("mouseover", () => {
+        tile.style.filter = darken(tile.style.filter);
+    });
+
     board.append(tile);
+}
+
+function darken(currentBright) {
+    num = currentBright.substring(currentBright.indexOf("(")+1, currentBright.length-1);
+    level = parseFloat(num) - 0.2;
+    if (level < 0) {
+        level = 0;
+    }
+
+    return `Brightness(${level})`;
 }
